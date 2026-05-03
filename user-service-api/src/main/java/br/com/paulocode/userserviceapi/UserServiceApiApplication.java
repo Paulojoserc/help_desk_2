4694e05e6b176065fa48810b92543f996564907c
Paulo Costa <pjrc83@gmail.com>
2026-05-02 23:09:52 -0300

    feat: add user repository

--- user-service-api/src/main/java/br/com/paulocode/userserviceapi/UserServiceApiApplication.java
+++ user-service-api/src/main/java/br/com/paulocode/userserviceapi/UserServiceApiApplication.java
@@ -1,13 +1,21 @@
 package br.com.paulocode.userserviceapi;
 
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class UserServiceApiApplication {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(UserServiceApiApplication.class, args);
     }
+
 
 }
